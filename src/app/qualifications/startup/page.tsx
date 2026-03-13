d2c377f3630951028eecd51c7429ba9d3c68734d
Logan Kay <logan@kaleoshq.com>
2026-03-13 14:41:04 -0600

    fix double arrow

--- src/app/qualifications/startup/page.tsx
+++ src/app/qualifications/startup/page.tsx
@@ -330,9 +330,6 @@ export default function StartupQualifications() {
               }}
             >
               Join the Waitlist &rarr;
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14M12 5l7 7-7 7" />
-              </svg>
             </Link>
           </motion.div>
         </div>
